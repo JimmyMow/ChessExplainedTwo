4c6e2d97c7df965ec93522e155fd0e19685bd95e
Jack Mallers <jackmallers@Jacks-MacBook-Pro.local>
2014-09-04 17:02:55 -0500

    switched to sendgrid address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'http://mighty-sierra-5164.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: "587",
     domain: "heroku.com",
     authentication: "plain",
