01f7a8efdf5ebab7e108f83091d771a1e77124df
Jack Mallers <jimmymowschess@gmail.com>
2014-10-20 19:20:56 -0500

    added only owners can edit the game details

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy, :review]
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:create, :update]
   before_action :check_for_user, only: [:new]
 
   # GET /games
