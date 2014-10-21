a4e5eca7ab5b634922a8f87c90e65836a71a8f22
Jack Mallers <jimmymowschess@gmail.com>
2014-10-21 15:44:11 -0500

    starting messaging over

--- db/migrate/20140904001729_add_columns_to_invitation.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToInvitation < ActiveRecord::Migration
-  def change
-    add_column :invitations, :scheduled_at, :datetime
-    add_column :invitations, :accepted, :boolean
-  end
-end
