bd7adabe23e522e5d62cf8754e93063a5b903a12
Jack Mallers <jimmymowschess@gmail.com>
2014-11-11 14:27:04 -0600

    fixed a few bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   after_filter :log_user_online_after_sign_up, only: [:create]
 
   def log_user_online_after_sign_up
-    current_user.is_now_online
+    current_user.is_now_online if current_user
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,10 @@ class SessionsController < Devise::SessionsController
   private
 
   def log_user_offline
-    current_user.is_now_offline
+    current_user.is_now_offline if current_user
   end
 
   def log_user_online
-    current_user.is_now_online
+    current_user.is_now_online if current_user
   end
-
 end
